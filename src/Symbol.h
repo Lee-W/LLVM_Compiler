5a83b3c54a89c9585fde10b10b8df2b0f947867c
LeeW <cl87654321@gmail.com>
2015-06-16 21:02:20 +0800

    Add Symbol.h

---
+++ src/Symbol.h
@@ -0,0 +1,27 @@
+#ifndef SYMBOL_H
+#define SYMBOL_H
+
+#include <iostream>
+using namespace std;
+
+
+struct Symbol {
+    Symbol() {}
+
+    Symbol(int s, string sy, string t, bool isA, bool isF)
+    {
+        scope = s;
+        symbol = sy;
+        type = t;
+        isArray = isA;
+        isFunction = isF;
+    }
+
+    int scope;
+    string symbol;
+    string type;
+    bool isArray;
+    bool isFunction;
+};
+
+#endif
