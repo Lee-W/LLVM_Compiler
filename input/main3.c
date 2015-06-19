081affb3eddcbbab853d9b81afc87649fa52da2a
LeeW <cl87654321@gmail.com>
2015-06-20 00:24:17 +0800

    Add main3.c main4.c as test case

---
+++ input/main3.c
@@ -0,0 +1,10 @@
+int main(int a[])
+{
+    if (true) {
+        double a;
+        a = 5.5;
+    } else {
+        double b;
+        b = 5;
+    }
+}
---
+++ input/main4.c
@@ -0,0 +1,23 @@
+int x ;
+
+int plus ( int a , int b , int d[]) {
+    int c ;
+    c = a + b ;
+    return 0 ;
+}
+
+int main (  ) {
+    int y [ 2 ] ;
+    y [ 0 ]  = 3 ;
+    y [ 1 ]  = 4 ;
+    x = plus( y [ 0 ] , y [ 1 ] ) ;
+    if ( x ) {
+        double a;
+        a = 0;
+    }
+    else {
+        float b ;
+        b = 0;
+    }
+    print x ;
+}
