835db5634a56ee9ea0f7c2a44b0b026c5a6f9cd4
LeeW <cl87654321@gmail.com>
2015-06-25 21:08:47 +0800

    Reset test case main.c

--- input/main.c
+++ input/main.c
@@ -1,22 +1,9 @@
-
-int test (int h, int i, double j){
-	j = h + i;
-}
-
-int b;
-int main () {
-	int a;
-	int c ;
-	int d;
-	
+int a ; 
+int main ( ) {
+	int b ;
+	int c  ;
 	a  =  0 ;
 	b =  1 ;
-	c  =  a + b + 2;
-	d = test (a,c,d);
-	if (a < b) {
-		b = 3;
-	}
-	//print a;
-	//print b;
+	c  =  a + b ;
 	return 0 ;
 }
